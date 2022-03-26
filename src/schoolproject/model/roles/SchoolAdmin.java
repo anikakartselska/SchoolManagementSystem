699236cee82908b9ef766bab63bb7accee9ae30d
Anika Kartselska <anika1931@abv.bg>
2022-03-26 23:48:05 +0200

    HeadTeacher Repository and SchoolAdmin repository updated

--- src/schoolproject/model/roles/SchoolAdmin.java
+++ src/schoolproject/model/roles/SchoolAdmin.java
@@ -1,11 +1,12 @@
 package schoolproject.model.roles;
 
+import schoolproject.dao.UserIdentifiable;
 import schoolproject.model.enums.Gender;
 import schoolproject.model.enums.Role;
 
 import java.util.Date;
 
-public class SchoolAdmin extends User{
+public class SchoolAdmin extends User implements UserIdentifiable {
 
     public SchoolAdmin(String firstName, String secondName, String thirdName, String email, String username, String password, Gender gender) {
         super(firstName, secondName, thirdName, email, username, password, gender);
